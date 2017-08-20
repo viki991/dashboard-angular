69b27c370e0e8e2b4241e2b9a91681d6dfe5bee0
AquiburRKhan <ark.aquib17@gmail.com>
2017-08-20 13:05:26 +0600

    add: added search on press enter key

--- src/app/pages/services/bitcoinService/bitcoinService.module.js
+++ src/app/pages/services/bitcoinService/bitcoinService.module.js
@@ -4,7 +4,7 @@
     angular.module('BlurAdmin.pages.services.bitcoinService', [
       "BlurAdmin.pages.services.bitcoinService.bitcoinServiceTransactions",
       "BlurAdmin.pages.services.bitcoinService.bitcoinServiceUsers",
-      "BlurAdmin.pages.services.bitcoinService.bitcoinServiceSettings",
+      "BlurAdmin.pages.services.bitcoinService.bitcoinServiceSettings"
     ])
         .config(routeConfig);
 
