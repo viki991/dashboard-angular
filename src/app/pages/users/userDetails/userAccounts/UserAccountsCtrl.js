cf256e744b6f64676dc51c88a1d7e399a08bfb1b
AquiburRKhan <ark.aquib17@gmail.com>
2017-10-04 17:07:12 +0600

    add: added functionality to add currency to user account

--- src/app/pages/users/userDetails/userAccounts/UserAccountsCtrl.js
+++ src/app/pages/users/userDetails/userAccounts/UserAccountsCtrl.js
@@ -5,13 +5,16 @@
         .controller('UserAccountsCtrl', UserAccountsCtrl);
 
     /** @ngInject */
-    function UserAccountsCtrl($rootScope,$scope,environmentConfig,$stateParams,$http,cookieManagement,errorHandler,$location,$state) {
+    function UserAccountsCtrl($rootScope,$scope,environmentConfig,$stateParams,_,
+                              $http,cookieManagement,errorHandler,$location,$state) {
 
         var vm = this;
         vm.token = cookieManagement.getCookie('TOKEN');
         vm.uuid = $stateParams.uuid;
         vm.reference = '';
+        $scope.newAccountCurrencies = {list: []};
         $scope.loadingUserAccounts = true;
+        $scope.addingCurrencies = false;
 
         vm.getUser = function(){
             if(vm.token) {
@@ -24,7 +27,6 @@
                 }).then(function (res) {
                     $scope.loadingUserAccounts = false;
                     if (res.status === 200) {
-                        vm.reference = res.data.data.results[0].reference;
                         $scope.accounts = res.data.data.results;
                         $scope.account = res.data.data.results[0].user;
                         $scope.currencies = res.data.data.results[0].currencies;
@@ -37,6 +39,62 @@
             }
         };
         vm.getUser();
+
+        vm.getCompanyCurrencies = function(){
+            if(vm.token){
+                $http.get(environmentConfig.API + '/admin/currencies/?enabled=true', {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': vm.token
+                    }
+                }).then(function (res) {
+                    if (res.status === 200) {
+                        $scope.currencyOptions = res.data.data.results;
+                    }
+                }).catch(function (error) {
+                    errorHandler.evaluateErrors(error.data);
+                    errorHandler.handleErrors(error);
+                });
+            }
+        };
+        vm.getCompanyCurrencies();
+
+        $scope.addAccountCurrency = function(listOfCurrencies){
+
+            var arrayOfCurrencies = _.pluck(listOfCurrencies,'code');
+
+            if(vm.token) {
+                $scope.loadingUserAccounts = true;
+                $http.post(environmentConfig.API + '/admin/accounts/' + vm.reference + '/currencies/',{currencies: arrayOfCurrencies}, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': vm.token
+                    }
+                }).then(function (res) {
+                    console.log(res);
+                    if (res.status === 201) {
+                        $scope.newAccountCurrencies = {list: []};
+                        toastr.success('New currencies have been added to the account');
+                        $scope.toggleAddAccountCurrency();
+                        $scope.loadingUserAccounts = false;
+                    }
+                }).catch(function (error) {
+                    $scope.newAccountCurrencies = {list: []};
+                    $scope.loadingUserAccounts = false;
+                    errorHandler.evaluateErrors(error.data);
+                    errorHandler.handleErrors(error);
+                });
+            }
+        };
+
+        $scope.toggleAddAccountCurrency = function (account) {
+            if(account){
+                vm.reference = account.reference;
+            } else {
+                vm.reference = ''
+            }
+            $scope.addingCurrencies = !$scope.addingCurrencies;
+        };
 
         $scope.goToView = function(state,currency,email,account){
           $rootScope.selectedCurrency = currency;
